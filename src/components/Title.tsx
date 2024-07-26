451d683776d2ebf25042e8d2aa29c1e39748d95f
hellojoyworldz <hellojoyworldz@gmail.com>
2024-07-26 15:48:18 +0900

    feat: Title 컴포넌트 생성

---
+++ src/components/Title.tsx
@@ -0,0 +1,34 @@
+import styled from "styled-components";
+
+const TitleComponent = styled.header`
+  position: sticky;
+  top: 0;
+  padding: 3em 1em 2em 1em;
+  width: 100%;
+  background: linear-gradient(0deg, transparent, #fffda6);
+  box-sizing: border-box;
+
+  .in {
+    max-width: 800px;
+    margin: 0 auto;
+  }
+
+  && h1 {
+    margin: 0;
+    font-size: 1.8rem;
+    text-align: center;
+  }
+`;
+
+const Title = ({ as, children, title }) => {
+  return (
+    <TitleComponent as={as}>
+      <div className="in">
+        <h1>{title}</h1>
+        {children}
+      </div>
+    </TitleComponent>
+  );
+};
+
+export default Title;
