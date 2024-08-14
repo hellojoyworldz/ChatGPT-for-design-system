9e2c284e4157d01819d4f5c95b6d1be5e5e5f593
hellojoyworldz <hellojoyworldz@gmail.com>
2024-08-14 16:46:23 +0900

    feat: Button 컴포넌트 생성

---
+++ src/components/Button.tsx
@@ -0,0 +1,39 @@
+import styled from "styled-components";
+import { ButtonHTMLAttributes, HTMLAttributes, ReactNode } from "react";
+
+const ButtonComponent = styled.button`
+  padding: 0 12px;
+  height: 40px;
+  background: #fffda6;
+  border: 1px solid #333;
+  border-radius: 52px;
+  cursor: pointer;
+
+  &:disabled {
+    background: #ddd;
+    border: 0;
+    cursor: auto;
+  }
+`;
+
+interface ButtonComponentProps extends HTMLAttributes<HTMLButtonElement> {
+  type?: ButtonHTMLAttributes<HTMLButtonElement>["type"];
+  children?: ReactNode;
+  onClick?: () => Promise<void>;
+  className?: string;
+  disabled?: boolean;
+}
+
+const Button = ({
+  type = "button",
+  children,
+  ...props
+}: ButtonComponentProps) => {
+  return (
+    <ButtonComponent type={type} {...props}>
+      {children}
+    </ButtonComponent>
+  );
+};
+
+export default Button;
